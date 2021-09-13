68ea5288e67a587410780cac75c6f0d014fa9484
Arijit Biswas <75790020+Aftershock007@users.noreply.github.com>
2021-09-13 12:34:08 +0530

    cpp

--- 92.reverse-linked-list-ii.cpp
+++ 92.reverse-linked-list-ii.cpp
@@ -20,6 +20,27 @@ class Solution
 public:
     ListNode *reverseBetween(ListNode *head, int left, int right)
     {
+        if (!head || !head->next)
+            return head;
+        ListNode *dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode *It = dummy, *prevIt;
+        for (int i = 0; i < left; i++)
+        {
+            prevIt = It;
+            It = It->next;
+        }
+        ListNode *ItR = It, *prevR = prevIt;
+        for (int i = left; i <= right; i++)
+        {
+            ListNode *forward = ItR->next;
+            ItR->next = prevR;
+            prevR = ItR;
+            ItR = forward;
+        }
+        prevIt->next = prevR;
+        It->next = ItR;
+        return dummy->next;
     }
 };
 // @lc code=end
